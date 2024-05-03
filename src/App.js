ef179a78f82d4add3ee40273ae7ac4205d27e3b1
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-05-03 08:47:27 +0300

    complete code along

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom"; // An Outlet component is included within a component that has nested routes. It basically serves as a signal to that parent component that it will render various different components as its children, depending on what route a user visits. 
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <>
-      <header>
-        
-      </header>
-    </>
-  );
+function App(){
+    const [users, setUsers] = useState([]);
+
+    useEffect(() =>{
+        fetch("http://localhost:4000/users")
+        .then(r => r.json())
+        .then(data => setUsers(data))
+        .catch(error => console.error(error));
+    }, []);
+
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <Outlet context={users} />
+        </>
+    );
 };
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,36 @@
+import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
 
-const routes = [
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
-  }
+const routes = [ //  by entering our different route objects as an array associated with our App route's children key, we've set them up to render inside of our App component. That means that if we navigate to any of these nested routes — such as /login, for example — our App component will render with our Login component as a child component.
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+                children: [
+                    {
+                        path: "/profile/:id",
+                        element: <UserProfile />
+                    }
+                ]
+            }, 
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            }
+        ]
+    }
 ];
 
 export default routes;
